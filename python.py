2bb9335b86c93ed3283b2d8263db3531c42b2f3e
ram <ram@check.com>
2023-09-07 16:07:43 +0530

    this is first commit

---
+++ python.py
@@ -0,0 +1,15 @@
+list1=[10,40,34,45,67]
+list1.reverse()
+print ("list1 is : ",list1)
+
+list2=[24,45,67,93,53,75,25]
+list2.sort()
+print ("sort is : ",list2)
+
+list3=[]
+list3=list1.copy()
+print ("list3 : ",list3)
+
+myindex=list2[2:4]
+print ("my index number is : ",myindex)
+
